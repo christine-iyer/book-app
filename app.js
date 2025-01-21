6c2f8a8d3b5512347ecd7926d4cd4f24fdff3612
chrisIyer <hellohotbox420@gmail.com>
2025-01-20 19:11:35 -0500

    updated database connection.

--- app.js
+++ app.js
@@ -3,22 +3,16 @@ require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const userRoutes = require('./routes/api/user')
-<<<<<<< Updated upstream
 const profileRoutes = require('./routes/api/profile')
-=======
 const reviewRoutes = require('./routes/api/review')
 
->>>>>>> Stashed changes
 
 const app = express()
 
 app.use(morgan('tiny'));
 app.use(express.json())
-<<<<<<< Updated upstream
-=======
 app.use('/api/user', userRoutes )
 app.use('/api/review', reviewRoutes )
->>>>>>> Stashed changes
 
 app.use('/api/user', userRoutes)
 app.use('/api/profile', profileRoutes)
