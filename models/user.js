e5a2fa9507c9aae870369dec2339ee90924db073
chrisIyer <hellohotbox420@gmail.com>
2025-01-30 12:54:46 -0500

    lets look at dev

--- models/user.js
+++ models/user.js
@@ -1,26 +1,13 @@
-
-const { Schema, model } = require('mongoose')
+const mongoose = require('mongoose'); 
+const { Schema, model, models } = mongoose; // Import models to check if User is already defined
 
 const userSchema = new Schema({
-     name: { type: String, required: true }
-})
-      username: { type: String, required: true },
-      email: {
-       type: String,
-       unique: true,
-       trim: true,
-       lowercase: true,
-       required: true
-     },
-     password: {
-       type: String,
-       trim: true,
-       minlength: 3,
-       required: true
-     },
-     profile: [{type: Schema.Types.ObjectId, ref: "Profile"}]
-   }, {
-     timestamps: true,
+  username: { type: String, required: true },
+  email: { type: String, unique: true, trim: true, lowercase: true, required: true },
+  password: { type: String, trim: true, minlength: 3, required: true }
+}, { timestamps: true });
 
-   })
-module.exports = model('User', userSchema)
+// Check if model already exists to avoid OverwriteModelError
+const User = models.User || model('User', userSchema);
+
+module.exports = User;
